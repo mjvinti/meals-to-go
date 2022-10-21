9e3fd2291f947b1097d06d44b414fa440418a301
Patricia Vinti <patriciavinti@Patricias-MBP.fios-router.home>
2022-10-21 16:19:40 -0400

    created initial scaffolding

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Our React Native Blank Canvas</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +13,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
