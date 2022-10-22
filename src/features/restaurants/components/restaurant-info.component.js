768881269355db373d9ef42a365035c8e1b4ce16
Mario Vinti <mariovinti@Patricias-MBP.fios-router.home>
2022-10-21 21:41:23 -0400

    did more work on restaurant info

--- src/features/restaurants/components/restaurant-info.component.js
+++ src/features/restaurants/components/restaurant-info.component.js
@@ -1,3 +1,16 @@
 import { Text } from "react-native";
 
-export const RestaurantInfo = () => <Text>RestaurantInfo</Text>;
+export const RestaurantInfo = ({ restaurant = {} }) => {
+  const {
+    name = "some restaurant",
+    icon,
+    photos = [
+      "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+    ],
+    address = "100 some random street",
+    isOpenNow = true,
+    rating = 4,
+    isClosedTemporarily,
+  } = restaurant;
+  return <Text>RestaurantInfo</Text>;
+};
