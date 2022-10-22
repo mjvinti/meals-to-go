c02b4b01e285b56da716398992388d8ef3e5af6b
Mario Vinti <mariovinti@Patricias-MBP.fios-router.home>
2022-10-21 22:15:15 -0400

    installed and using styled components

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,6 +1,13 @@
 import { StyleSheet, Text } from "react-native";
 import { Card } from "react-native-paper";
 
+import styled from "styled-components/native";
+
+const Title = styled.Text`
+  padding: 16px;
+  color: red;
+`;
+
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = "some restaurant",
@@ -13,10 +20,11 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
     rating = 4,
     isClosedTemporarily,
   } = restaurant;
+
   return (
     <Card elevation={5} style={styles.card}>
       <Card.Cover key={name} style={styles.cover} source={{ uri: photos[0] }} />
-      <Text style={styles.title}>{name}</Text>
+      <Title>{name}</Title>
     </Card>
   );
 };
@@ -24,5 +32,4 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
 const styles = StyleSheet.create({
   card: { backgroundColor: "white" },
   cover: { padding: 20, backgroundColor: "white" },
-  title: { padding: 16 },
 });
