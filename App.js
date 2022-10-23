ab47da6be7d46471d14e682f961ce66d6cd70daf
Mario Vinti <mjvinti@gmail.com>
2022-10-23 14:55:39 -0400

    set up tab navigation

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import { Text } from "react-native";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { ThemeProvider } from "styled-components/native";
 import {
@@ -5,10 +6,26 @@ import {
   Oswald_400Regular,
 } from "@expo-google-fonts/oswald";
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
+import { SafeArea } from "./src/components/utils/safe-area.component";
 
 import { theme } from "./src/infrastructure/theme";
+
+const Tab = createBottomTabNavigator();
+
+const SettingsScreen = () => (
+  <SafeArea>
+    <Text>Settings</Text>
+  </SafeArea>
+);
+const MapsScreen = () => (
+  <SafeArea>
+    <Text>Maps</Text>
+  </SafeArea>
+);
 
 export default function App() {
   const [oswaldLoaded] = useOswald({ Oswald_400Regular });
@@ -21,7 +38,13 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <RestaurantsScreen />
+        <NavigationContainer>
+          <Tab.Navigator>
+            <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+            <Tab.Screen name="Maps" component={MapsScreen} />
+            <Tab.Screen name="Settings" component={SettingsScreen} />
+          </Tab.Navigator>
+        </NavigationContainer>
         <ExpoStatusBar style="auto" />
       </ThemeProvider>
     </>
--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,15 +1,11 @@
-import { FlatList, SafeAreaView, StatusBar } from "react-native";
+import { FlatList } from "react-native";
 import { Searchbar } from "react-native-paper";
 
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
+import { SafeArea } from "../../../components/utils/safe-area.component";
 import { Spacer } from "../../../components/spacer/spacer.component";
 
 import styled from "styled-components/native";
-
-const SafeArea = styled(SafeAreaView)`
-  flex: 1;
-  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
-`;
 
 const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
@@ -27,7 +23,14 @@ export const RestaurantsScreen = () => (
       <Searchbar />
     </SearchContainer>
     <RestaurantList
-      data={[{ name: 1 }, { name: 2 }, { name: 3 }, { name: 4 }]}
+      data={[
+        { name: 1 },
+        { name: 2 },
+        { name: 3 },
+        { name: 4 },
+        { name: 5 },
+        { name: 6 },
+      ]}
       renderItem={() => (
         <Spacer position="bottom" size="large">
           <RestaurantInfoCard />
